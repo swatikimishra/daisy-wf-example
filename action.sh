e73fb7251bceb937cd51dc5d2eaefe9d5961139b
swatikimishra <89043654+swatikimishra@users.noreply.github.com>
2021-08-22 14:28:04 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
 git clone -b $branch https://github.com/TeamDaisyX/DaisyXMusic
-cp DaisyXMusic/.env /root/TeamDaisyX/.env
+cp DaisyXMusic/local.env /root/TeamDaisyX/local.env
 cd /root/DaisyXMusic
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t DaisyXMusic
 docker run --privileged --env-file .env --rm -i DaisyXMusic
