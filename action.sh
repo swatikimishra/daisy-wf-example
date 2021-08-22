dd09b67a059af1924d59346d513bfb1eb9c1b1d0
swatikimishra <89043654+swatikimishra@users.noreply.github.com>
2021-08-22 13:42:31 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=main
 git clone -b $branch https://github.com/TeamDaisyX/DaisyXMusic
-cp ultroid/.env /root/TeamUltroid/.env
-cd /root/TeamUltroid
-docker build . --rm --force-rm --compress --pull --file Dockerfile -t ultroid
-docker run --privileged --env-file .env --rm -i ultroid
+cp DaisyXMusic/.env /root/DaisyXMusic/.env
+cd /root/DaisyXMusic
+docker build . --rm --force-rm --compress --pull --file Dockerfile -t DaisyXMusic
+docker run --privileged --env-file .env --rm -i DaisyXMusic
