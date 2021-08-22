eca7d1f0e335ce8c28bcf1f6d7895ef3b26e66ad
swatikimishra <89043654+swatikimishra@users.noreply.github.com>
2021-08-22 15:14:16 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,6 +1,6 @@
 branch=Py-Tgcalls
 git clone -b $branch https://github.com/TeamDaisyX/DaisyXMusic
-cp DaisyXMusic/example.env /root/TeamDaisyX/local.env
-cd /root/DaisyXMusic
+cp root/example.env /root/local.env
+cd /root/
 docker build . --rm --force-rm --compress --pull --file Dockerfile -t DaisyXMusic
 docker run --privileged --env-file local.env --rm -i DaisyXMusic
